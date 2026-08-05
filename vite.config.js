95205a35ab2efc7336d91c2496b7960e658be688
user2724 <jcgsummer4@gmail.com>
2026-08-05 16:06:43 -0400

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  //base: '/user2724/My-React-App',
+  plugins: [react()],
+})
